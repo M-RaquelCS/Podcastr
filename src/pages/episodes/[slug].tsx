61a6e52a83cae5dab3366c65fc2e1cc65be434d6
M-RaquelCS <M-Raquel@outlook.com.br>
2021-04-24 00:11:21 -0300

    funcionalidades do Player, adicionando episodios e controlando play and oause

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -28,6 +28,7 @@ type EpisodeProps = {
 }
 //astronautas
 export default function Episode({episode}: EpisodeProps){
+
     return(
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -55,9 +56,24 @@ export default function Episode({episode}: EpisodeProps){
 }
 
 export const getStaticPaths: GetStaticPaths = async() =>{
+    const { data } = await api.get('episodes', {
+        params: {
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc',
+        }
+    })
+
+    const paths = data.map(episode =>{
+        return{
+            params:{
+                slug: episode.id
+            }
+        }
+    })
     return{
-        paths: [],
-        fallback:'blocking'
+        paths,
+        fallback: 'blocking' //incremental static regeneration
     }
 }
 
